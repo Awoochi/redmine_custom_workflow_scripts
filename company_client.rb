a262ac22f08589d231f230015d025359f9bcc9a3
SENPAI4HIRE <ishir093@gmail.com>
2019-10-08 17:43:37 +0500

    Helpdesk project check was added

--- company_client.rb
+++ company_client.rb
@@ -1,8 +1,14 @@
 #This script automatically update custom field value when it's not equal a current project id
 
 cf = CustomField.find(32)
+cus_com = @issue.customer_company
+cf_name = cf.enumerations.find_by(name: cus_com)
 
 if subject.present?
+  if @issue.project_id == 8 # If project is "Helpdesk" then change custom field value on customer company value
+    @issue.custom_field_values = {cf.id => cf_name.id}
+  end
+
   if cf.id != @issue.project_id
     @issue.custom_field_values = { cf.id => @issue.project_id }
   end
